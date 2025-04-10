b6052b6d1b304f2143367896ae982b236a3aec10
eduardoXavier360 <eduardokungfu01@hotmail.com>
2025-04-09 23:19:48 -0300

    Personalização_caneca

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const cacheName = "personalize-presente-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js")
+      .then(() => console.log("Service Worker registrado!"))
+      .catch(error => console.error("Erro ao registrar o Service Worker:", error));
+  }
+  
